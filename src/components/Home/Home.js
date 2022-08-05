8f81c89eb365851181ac70295a4c106905b90eac
MrGozXD <yannis.peloutier@ensea.fr>
2022-08-05 15:25:38 +0200

    +Sign up form

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,18 @@
 import React from "react";
+import Userfront from "@userfront/react";
+
+Userfront.init("6nz4zp8n"); //identifiant compte Userfront
+
+const SignupForm = Userfront.build({
+  toolId: "bkdmon",
+});
 
 export default function Home() {
-    return <h2>Home</h2>;
+    return (
+        <div>
+            <h2>Home</h2>
+            <SignupForm/>
+        </div>
+    )
 }
 
