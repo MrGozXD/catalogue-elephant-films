c71f497d2eb55a77fce69e8e80cb600b2f30f1b7
MrGozXD <yannis.peloutier@ensea.fr>
2022-08-05 15:49:34 +0200

    + Dashboard userData

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Userfront from "@userfront/react";
 
 export default function Dashboard() {
-    return <h2>Dashboard</h2>;
+    const userData = JSON.stringify(Userfront.user, null, 2);
+    return (
+      <div>
+        <h2>Dashboard</h2>
+        <pre>{userData}</pre>
+        <button onClick={Userfront.logout}>Logout</button>
+      </div>
+    );
 }
