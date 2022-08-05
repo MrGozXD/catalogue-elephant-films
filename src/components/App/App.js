f2b4996882e06b2b7910a25f8daef1c9dc5e167b
MrGozXD <yannis.peloutier@ensea.fr>
2022-08-05 13:35:02 +0200

    +components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,10 @@
 import logo from './Logo_Elephant-Noir.png'
 import './App.css';
+import { BrowserRouter as Router,Routes, Route, Link } from "react-router-dom";
+import Home from '../Home/Home.js';
+import Login from '../Login/Login.js';
+import PasswordReset from '../PasswordReset/PasswordReset';
+import Dashboard from '../Dashboard/Dashboard';
 
 function App() {
   return (
@@ -8,7 +13,35 @@ function App() {
         <p>Bienvenue sur le catalogue de </p>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <section></section>
+      <section>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/reset">Reset</Link>
+                </li>
+                <li>
+                  <Link to="/Dashboard">Dashboard</Link>
+                </li>
+              </ul>
+            </nav>
+
+            <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="/login" element={<Login />}/>
+              <Route path="/reset" element={<PasswordReset />}/>
+              <Route path="/dashboard" element={<Dashboard />}/>
+            </Routes>
+          </div>
+        </Router>
+      </section>
       <footer className="App-footer">
         <p>Bas de page</p>
       </footer>
@@ -17,3 +50,5 @@ function App() {
 }
 
 export default App;
+
+
---
+++ src/components/PasswordReset/PasswordReset.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PasswordReset() {
+    return <h2>Password Reset</h2>;
+  }
