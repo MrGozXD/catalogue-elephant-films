42ff6c3aa079743ea1a473229aef50e815d0eaab
MrGozXD <yannis.peloutier@ensea.fr>
2022-08-05 15:57:26 +0200

    +Require Authentification

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/App.js';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate,useLocation  } from "react-router-dom";
+import Userfront from "@userfront/react";
+import App from './components/App/App.js';
 import Home from './components/Home/Home.js';
 import PasswordReset from './components/PasswordReset/PasswordReset.js';
 import Dashboard from './components/Dashboard/Dashboard.js';
 import Login from './components/Login/Login';
-import Error from './components/Error/Error'
+import Error from './components/Error/Error';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,13 +21,22 @@ root.render(
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/passwordreset" element={<PasswordReset />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} />
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
 
+function RequireAuth({ children }) {
+  let location = useLocation();
+  if (!Userfront.tokens.accessToken) {
+    // Redirect to the /login page
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
