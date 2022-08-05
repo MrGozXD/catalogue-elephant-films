35ce983fecae0ae41c7e18ee4964cd4b3439da00
MrGozXD <yannis.peloutier@ensea.fr>
2022-08-05 15:35:13 +0200

    +Log In Form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,18 @@
 import React from "react";
+import Userfront from "@userfront/react";
+
+Userfront.init("6nz4zp8n");
+
+const LoginForm = Userfront.build({
+    toolId: "onlabo"
+});
 
 export default function Login() {
-    return <h2>Login</h2>;
+    return (
+        <div>
+            <h2>Login</h2>
+            <LoginForm />
+        </div>
+    )
 }
+
