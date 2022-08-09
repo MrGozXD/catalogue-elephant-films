ced51e49fe24f1ab331afb41c30109c2e9bfba63
MrGozXD <yannis.peloutier@ensea.fr>
2022-08-09 15:01:55 +0200

    papa parse fonctionnel

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,8 +5,8 @@ import { parsePath } from "history";
 const Papa = require('papaparse');
 const Tabletop = require('tabletop');
 
-var publicSpreadsheetUrl = '126kFg-K5C5uQoGR6fVGYhThdLY31HcDjfxaThD4LdY4';
-
+var publicSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT1QGEpIxLSwBBJ-bbUEhsQuDOxMosn0fX6xTkHN7xGEcaYXa8bBihTNfHcevcf_7SokkepX0tMtnFW/pub?gid=0&single=true&output=csv";
+var catalogue;
 function showInfo(data, tabletop) {
     // do something with the data
     console.log("Hello World ! dans showInfo");
@@ -15,10 +15,12 @@ function showInfo(data, tabletop) {
 
 function init() {
     Papa.parse(publicSpreadsheetUrl, {
+        worker: true,
         download: true,
         header: true,
         complete: function (results) {
-            var data = results.data
+            var data = results.data;
+            global.catalogue=data;
             console.log(data)
         }
     })
@@ -31,10 +33,12 @@ export default function Dashboard() {
     console.log("Dashboard");
     init();
     const userData = JSON.stringify(Userfront.user, null, 2);
+    const catalogueFinal = JSON.stringify(global.catalogue[2200],null,2);
     return (
         <div>
             <h2>Dashboard</h2>
             <pre>{userData}</pre>
+            <pre>{catalogueFinal}</pre>
             <button onClick={Userfront.logout}>Logout</button>
         </div>
     );
