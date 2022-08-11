7466dd0fc512ed96e7fb496ad50218ab67251a74
MrGozXD <yannis.peloutier@ensea.fr>
2022-08-11 15:58:14 +0200

    SearchBar init

---
+++ src/components/Searchbar/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SearchBar() {
+  return (
+    <div className='search'>
+        <div className='searchInputs'>
+            <input type="text" />
+            
+        </div>
+        <div className='dataResults'></div>
+    </div>
+  )
+}
+
+export default SearchBar
