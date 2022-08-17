514b4a65917e0a04d07e4000f622b0f6bcb3ec70
MrGozXD <yannis.peloutier@ensea.fr>
2022-08-17 16:26:58 +0200

    SearchBar Nom+EAN

--- src/components/Searchbar/SearchBar.js
+++ src/components/Searchbar/SearchBar.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./SearchBar.css";
+import SearchIcon from '@mui/icons-material/Search';
 
-function SearchBar() {
+function SearchBar({ placeholder, data }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const handleFilter =(event)=>{
+    const searchWord = event.target.value;
+    const newFilter = data.filter((value)=>{
+      return (
+        value.Nom.toLowerCase().includes(searchWord.toLowerCase()) || value['Code EAN'].includes(searchWord) 
+      ) 
+        /* filtre à réécrire => return true si searchWord match avec value.critèreX || value.critèreY || etc */
+    });
+    setFilteredData(newFilter);
+  };
+
+
+
   return (
-    <div className='search'>
-        <div className='searchInputs'>
-            <input type="text" />
-            
+    <div className="search">
+      <div className="searchInputs">
+        <input type="text" placeholder={placeholder} onChange={handleFilter} />
+        <div className="searchIcon">
+          <SearchIcon />
         </div>
-        <div className='dataResults'></div>
+      </div>
+      {filteredData.length !==0 && (
+        <div className="dataResult">
+          {filteredData.map((value, key) => {
+            return (
+              <a className="dataItem" href='https://github.com/MrGozXD' target="_blank">
+                <p>{value.Nom} EAN :{value['Code EAN']}</p> 
+              </a>
+            );
+          })}
+        </div>
+      )}
     </div>
   )
 }
