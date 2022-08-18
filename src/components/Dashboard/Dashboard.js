2438c9906e8f6f039f4e3fd0fc6b03f757055388
MrGozXD <yannis.peloutier@ensea.fr>
2022-08-18 14:01:11 +0200

    -unused variables -display data

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,17 +1,9 @@
 import React from "react";
 import Userfront from "@userfront/react";
-import { parsePath } from "history";
 import SearchBar from "../Searchbar/SearchBar.js";
-const Papa = require('papaparse');
-const Tabletop = require('tabletop');
 
+const Papa = require('papaparse');
 const publicSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT1QGEpIxLSwBBJ-bbUEhsQuDOxMosn0fX6xTkHN7xGEcaYXa8bBihTNfHcevcf_7SokkepX0tMtnFW/pub?gid=0&single=true&output=csv";
-let catalogue=[];
-function showInfo(data, tabletop) {
-    // do something with the data
-    console.log("Hello World ! dans showInfo");
-    console.log(JSON.stringify(data, null, 2));
-}
 
 function init() {
     Papa.parse(publicSpreadsheetUrl, {
@@ -30,14 +22,12 @@ function init() {
 export default function Dashboard() {
     console.log("Dashboard");
     init();
-    const userData = JSON.stringify(Userfront.user, null, 2);
-    const catalogueFinal = JSON.stringify(global.catalogue[1],null,2);
+    //const userData = JSON.stringify(Userfront.user, null, 2);
+    //const catalogueFinal = JSON.stringify(global.catalogue[1],null,2);
     return (
         <div>
             <h2>Dashboard</h2>
             <SearchBar placeholder="Recherche..." data={global.catalogue}/>
-            <pre>{userData}</pre>
-            <pre>{catalogueFinal}</pre>
             <button onClick={Userfront.logout}>Logout</button>
         </div>
     );
