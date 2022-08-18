7334f8d1a6a3d5eaf6470371c47205ac67ea8263
MrGozXD <yannis.peloutier@ensea.fr>
2022-08-18 14:01:39 +0200

    affichage fiche produit

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,37 @@
+import React,{useEffect,useRef} from 'react'
+import "./Modal.css"
+
+function Modal(props) {
+    
+    const{visible,cancel,children}=props;
+    const modalRef = useRef(null);
+
+    useEffect(() => {
+      const checkIfClickedOutside=(e)=>{
+        if (visible && modalRef.current && !modalRef.current.contains(e.target)){
+            cancel();
+        }
+      };
+
+      document.addEventListener("mousedown",checkIfClickedOutside);
+    
+      return () => {
+        document.removeEventListener("mousedown",checkIfClickedOutside);
+      };
+    }, [visible]);
+
+    return(
+        <>
+            {visible &&(
+                <div className="modal-bg">
+                    <div ref={modalRef} className="modal-container">
+                        <button onClick={cancel}>close modal</button>
+                        {children}
+                    </div>
+                </div>
+            )}
+        </>
+    ); 
+};
+
+export default Modal
