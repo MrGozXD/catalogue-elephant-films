808380db776bc13e54db15e79525958f3c444236
MrGozXD <yannis.peloutier@ensea.fr>
2022-08-25 11:55:19 +0200

    header admin

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import { Link, Outlet} from "react-router-dom";
+import Userfront from "@userfront/react";
 import "./Header.css"
 import logo from "./Logo_Elephant-Fond_Fonce.png"
 
 function Header() {
-  return(
-    <div className="top">
-      <div className="topLeft">
-        <img src={logo} className="App-logo" alt="logo" />
+  if (Userfront.user["email"]==="giratina@pokemon.com") {
+    return(
+      <div className="top">
+        <div className="topLeft">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className="topCenter">
+          <ul className="topList">
+            <Link to="/home">Créer un compte </Link>
+            <Link to="/login">Login </Link>
+            <Link to="/passwordreset">Reset </Link>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/dashboardAdmin">Admin</Link>
+            <Outlet />
+          </ul>
+        </div>
+        <div className="topRight">
+          <h1>Hello</h1>
+        </div>
       </div>
-      <div className="topCenter">
-        <ul className="topList">
-          <Link to="/home">Créer un compte </Link>
-          <Link to="/login">Login </Link>
-          <Link to="/passwordreset">Reset </Link>
-          <Link to="/dashboard">Dashboard</Link>
-          <Outlet />
-        </ul>
+    );
+  }else{
+    return(
+      <div className="top">
+        <div className="topLeft">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className="topCenter">
+          <ul className="topList">
+            <Link to="/">Accueil </Link>
+            <Link to="/passwordreset">Reset </Link>
+            <Outlet />
+          </ul>
+        </div>
+        <div className="topRight">
+          <h1>Hello</h1>
+        </div>
       </div>
-      <div className="topRight">
-        <h1>Hello</h1>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Header 
