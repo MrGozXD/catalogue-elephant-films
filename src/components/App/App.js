bb23657571593b68d3654215d2f8c566e26325c2
MrGozXD <yannis.peloutier@ensea.fr>
2022-08-26 16:22:26 +0200

    style page accueil fonctionnel

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import logo from './Logo_Elephant-Noir.png'
+import logo from '../Header/Logo_Elephant-Fond_Fonce.png'
 import './App.css';
 import { Link, Outlet } from "react-router-dom";
 import Userfront from "@userfront/react";
@@ -6,23 +6,23 @@ import Userfront from "@userfront/react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <p>Bienvenue sur le catalogue de </p>
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <article>
-        <div>
+      </div>
+      <div className="App-wrapper">
+        <div className="App-links">
           <Link to="/home">Home </Link>
           <Link to="/login">Login </Link>
           <Link to="/passwordreset">Reset </Link>
           <Link to="/dashboard">Dashboard</Link>
           <Outlet />
         </div>
-      </article>
-      <footer className="App-footer">
-        <button onClick={Userfront.logout}>Logout</button>
+      </div>
+      <div className="App-footer">
+        <button className="buttonLogout" onClick={Userfront.logout}>Logout</button>
         <p>Bas de page</p>
-      </footer>
+      </div>
     </div>
   );
 }
